6dff741cf23c6acb8efbfb284b8b8854b2154717
michaelatejeramiranda <106320064+michaelatejeramiranda@users.noreply.github.com>
2024-03-18 09:37:05 +0800

    Login and Signup

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require "php/connection.php"
+require "backend/connection.php"
 ?>
 
 
@@ -12,9 +12,9 @@ require "php/connection.php"
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link rel="shortcut icon" href="img/icons/logo.png" />
+	<link rel="shortcut icon" href="img/icons/logo-blue.png" />
 
-	<title>Seller Dashboard | SMDC JQB</title>
+	<title>Sign In | SDMC JQB</title>
 
 	<link href="css/app.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
@@ -22,235 +22,126 @@ require "php/connection.php"
 </head>
 
 <body>
-	<div class="wrapper">
-		<nav id="sidebar" class="sidebar js-sidebar">
-			<div class="sidebar-content js-simplebar">
-				<a class="sidebar-brand" href="index.php">
-          			<span class="d-flex align-middle justify-content-center"> <img class="smdc-logo" src="img/icons/logo.png" alt=""> </span>
-        		</a>
+	<main class="d-flex w-100" style="background: linear-gradient(rgba(0, 48, 255, .15), rgba(0, 48, 255, .15)), url(&quot;img/bg/bg.jpg&quot;);
+	background-size: cover;
+	background-position: center;">
 
-				<ul class="sidebar-nav">
-					<li class="sidebar-header">
-						Reports
-					</li>
+		<div class="container d-flex flex-column">
+			<div class="row vh-100">
+				<div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto d-table h-100">
+					<div class="d-table-cell align-middle">
+						<div class="card">
+							<div class="card-header pb-0" style="background-color: #0030FF;">
+								<div class="text-center">
+									<p class="lead fw-bold text-white">
+										Sign in to your account to continue
+									</p>
+								</div>
+							</div>
+							<div class="card-body">
+								<div class="row d-flex justify-content-center">
+									<div class="col-8">
+										<img src="img/icons/logo-blue.png" alt="logo" class="img-fluid">
+									</div>
+								</div>
+								<div class="m-sm-3">
+									<form action="backend/login.php" method="post" class="needs-validation" novalidate>
+										<div class="mb-3">
+											<label class="form-label">Email</label>
+											<input class="form-control form-control-lg" type="email" name="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : ''; ?>" placeholder="Enter your email" required>
+										</div>
+										<div class="mb-3">
+											<label class="form-label">Password</label>
+											<input class="form-control form-control- <?php echo isset($errors) ? 'is-invalid' : ''; ?>" type="password" name="password" placeholder="Enter your password" required>
+											<?php if (isset($errors)): ?>
+												<div class="invalid-feedback">
+													<?php echo $errors[0]; ?>
+												</div>
+											<?php endif; ?>
+										</div>
 
-					<li class="sidebar-item active">
-						<a class="sidebar-link" href="index.php">
-              			<i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
-            			</a>
-					</li>
+										<div class="row d-flex justify-content-between px-3">
+											<div class="form-check col-auto">
+												<input id="customControlInline" type="checkbox" class="form-check-input" value="remember-me" name="remember-me" checked>
+												<label class="form-check-label text-small" for="customControlInline">Remember me</label>
+											</div>
+											<div class="col-auto">
+												<a class="text-decoration-underline">
+													Forgot Password?
+												</a>
+											</div>
+										</div>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages/pages-statistics.php">
-              				<i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Statistics</span>
-            			</a>
-					</li>
+										<div class="row mt-3 mx-1">
+											<button type="submit" class="btn btn-lg btn-primary">Sign in</button>
+										</div>
 
-					<li class="sidebar-header">
-						Manage
-					</li>
+										<div class="row d-flex justify-content-start mt-4">
+											<div class="col-auto d-flex justify-content-start">
+												<p class="me-2">Dont have account?</p>
+												<a class="text-decoration-underline" href="pages/pages-sign-up.php">
+													Register Now
+												</a>
+											</div>
+										</div>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages/pages-sales.php">
-              				<i class="align-middle" data-feather="dollar-sign"></i> <span class="align-middle">Sales</span>
-            			</a>
-					</li>
+										<!-- <div class="mb-3">
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages/pages-booking.php">
-              				<i class="align-middle" data-feather="book"></i> <span class="align-middle">Booking</span>
-            			</a>
-					</li>
+											<label class="form-label">Role</label>
+                                                <select class="form-select" id="role" name="role">
+												<?php
+													$sql_roles = "SELECT * FROM roles";
+													$res_roles = mysqli_query($conn, $sql_roles);
 
+													if($res_roles == TRUE){
+													$count_get = mysqli_num_rows($res_roles);
+													if($count_get > 0){
+														while($rows_roles = mysqli_fetch_assoc($res_roles)){
+															$roles = $rows_roles['role'];   
+															?>
+															<option value="<?php echo $roles;?>"><?php echo $roles;?></option>
+															<?php
+														}
+													}
+													}
 
-					<li class="sidebar-header">
-						Settings
-					</li>
+												?>
+                                                </select>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages/pages-profile.php">
-              				<i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
-            			</a>
-					</li>
+										</div> -->
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="">
-              				<i class="align-middle" data-feather="settings"></i> <span class="align-middle">Settings</span>
-            			</a>
-					</li>
-
-				</ul>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-		</nav>
-
-		<div class="main">
-			<nav class="navbar navbar-expand navbar-light navbar-bg">
-				<a class="sidebar-toggle js-sidebar-toggle">
-          			<i class="hamburger align-self-center"></i>
-        		</a>
-
-				<div class="navbar-collapse collapse">
-					<ul class="navbar-nav navbar-align">
-
-						<li class="nav-item dropdown">
-							<a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-bs-toggle="dropdown">
-                			<i class="align-middle" data-feather="settings"></i>
-              				</a>
-
-							<a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-            				<span class="text-dark">User</span>
-              				</a>
-
-							<div class="dropdown-menu dropdown-menu-end">
-								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
-								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="#">Log out</a>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</nav>
-
-			<main class="content">
-				<div class="container-fluid p-0">
-
-					<h1 class="h3"><strong class="title-dashboard">Overview</strong> Dashboard</h1>
-
-					<div class="container mb-4">
-						<div class="row">
-							<div class="col text-end">
-								<a href="pages/pages-booking.php" class="btn btn-primary btn-lg background-blue">Book Now</a>
-							</div>
-						</div>
-					</div>
-					
-					<div class="row d-flex justify-content-center">
-						<div class="col-md-6 col-lg-4 mx-auto mb-4">
-							<div class="card">
-								<div class="card-body">
-									<div class="row">
-										<div class="col mt-0">
-											<h5 class="card-title">Total Sales</h5>
-										</div>
-
-										<div class="col-auto">
-											<div class="stat text-primary">
-												<i class="align-middle" data-feather="dollar-sign"></i>
-											</div>
-										</div>
-									</div>
-									<h1 class="mt-1 mb-3" style="font-weight: bold;"> 1,000,000 </h1>
-									<div class="mb-0">
-										<span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> </span>
-										<span class="text-muted" style="font-size: .85em;">Last 24 hours </span>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div class="col-md-6 col-lg-4 mx-auto mb-4">
-							<div class="card">
-								<div class="card-body">
-									<div class="row">
-										<div class="col mt-0">
-											<h5 class="card-title">Commissions</h5>
-										</div>
-
-										<div class="col-auto">
-											<div class="stat text-primary">
-												<i class="align-middle" data-feather="percent"></i>
-											</div>
-										</div>
-									</div>
-									<h1 class="mt-1 mb-3" style="font-weight: bold;"> 1,000,000 </h1>
-									<div class="mb-0">
-										<span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i>  </span>
-										<span class="text-muted" style="font-size: .85em;">Last 24 hours </span>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div class="col-md-6 col-lg-4 mx-auto mb-4">
-							<div class="card">
-								<div class="card-body">
-									<div class="row">
-										<div class="col mt-0">
-											<h5 class="card-title">Unit Sold</h5>
-										</div>
-
-										<div class="col-auto">
-											<div class="stat text-primary">
-												<i class="align-middle" data-feather="home"></i>
-											</div>
-										</div>
-									</div>
-									<h1 class="mt-1 mb-3" style="font-weight: bold;"> 3 </h1>
-									<div class="mb-0">
-										<span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> </span>
-										<span class="text-muted" style="font-size: .85em;">Last 24 hours </span>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						
-					</div>
-
-					<!-- <div class="row">
-						<div class="col-12 col-md-12 d-flex ">
-							<div class="card flex-fill w-100">
-								<div class="card-header">
-
-									<h5 class="card-title mb-0">Summary of sales</h5>
-								</div>
-
-							</div>
-						</div>
-					</div> -->
-
-					<div class="row">
-						<div class="col-12 col-md-12 d-flex">
-							<div class="card flex-fill">
-								<div class="card-header">
-
-									<h5 class="card-title mb-0">Latest Transaction</h5>
-								</div>
-								<table class="table table-hover my-0">
-									<thead>
-										<tr class="text-center">
-											<th>Name</th>
-											<th>Unit Code</th>
-											<th>Amount</th>
-											<th>Date</th>
-											<th>Account Type</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr class="text-center">
-											<td>Sample</td>
-											<td>Unit 1</td>
-											<td>2,500,000</td>
-											<td>03/14/2024</td>
-											<td>Regular</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>
-						
-					</div>
-
-				</div>
-			</main>
-
 		</div>
-	</div>
+	</main>
 
 	<script src="js/app.js"></script>
-	<script src="js/script.js"></script>
+	<script>
+		// Example starter JavaScript for disabling form submissions if there are invalid fields
+		(() => {
+		'use strict'
 
+		// Fetch all the forms we want to apply custom Bootstrap validation styles to
+		const forms = document.querySelectorAll('.needs-validation')
+
+		// Loop over them and prevent submission
+		Array.from(forms).forEach(form => {
+			form.addEventListener('submit', event => {
+			if (!form.checkValidity()) {
+				event.preventDefault()
+				event.stopPropagation()
+			}
+
+			form.classList.add('was-validated')
+			}, false)
+		})
+		})()
+	</script>
 
 </body>
 
